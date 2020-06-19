eda14b0fbfb50f5c5472e823606ee7cf3b8f7b95
Baxi19 <randald1991@gmail.com>
2020-06-18 19:58:35 -0600

    Grafo bi-direccional para laberinto listo

---
+++ laberinto.pl
@@ -0,0 +1,52 @@
+
+
+% se declara conocimento dinamico
+:-  dynamic conectado/2.
+:-  dynamic arco/2.
+
+conectado(X,Y):-
+    arco(X,Y);
+    arco(Y,X).
+
+% grafo en forma de matriz para el laberinto
+%
+
+arco(1,7).
+arco(7,13).
+arco(13,14).
+arco(14,15).
+arco(15,21).
+arco(21,22).
+arco(22,16).
+arco(16,10).
+arco(10,4).
+arco(4,3).
+arco(3,9).
+arco(3,2).
+arco(9,8).
+arco(8,2).
+arco(inicio,2).
+arco(14,20).
+arco(20,26).
+arco(26,27).
+arco(27,28).
+arco(28,29).
+arco(29,23).
+arco(23,17).
+arco(17,11).
+arco(11,5).
+arco(5,6).
+arco(28,34).
+arco(34,35).
+arco(35,36).
+arco(36,30).
+arco(30,24).
+arco(24,18).
+arco(18,12).
+arco(34,33).
+arco(33,32).
+arco(32,31).
+arco(31,25).
+arco(25,19).
+arco(32,fin).
+
